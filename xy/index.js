4e8a175b4aa869ce95772d0225416e32e3e98970
Yuqing CHEN <chenyuqing.one@bytedance.com>
2023-09-27 10:09:33 +0800

    support dir

--- xy/index.js
+++ xy/index.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
+const path = require('path');
 
-// You should replace the 'input.xy' with your real filename
-const input = fs.readFileSync('./input.xy').toString();
+// check if directory exists
+function directoryExists(dirPath) {
+  try {
+    return fs.statSync(dirPath).isDirectory();
+  } catch (err) {
+    return false;
+  }
+}
 
 // See https://en.wikipedia.org/wiki/Newline
 function getEOL(str) {
@@ -40,84 +47,127 @@ function getXY(str) {
 }
 
 
-const eol = getEOL(input);
+function processSingleFile(fileName) {
 
-// the origin string lines of the input file
-const lines = input.split(eol);
+  const filePath = path.resolve('./input', fileName);
+  const input = fs.readFileSync(filePath).toString();
+  const eol = getEOL(input);
 
-// current meta data of the region processed in each traverses
-let currentData = null;
+  // the origin string lines of the input file
+  const lines = input.split(eol);
 
-// The result data for generating output.csv
-const resultData = [];
+  // current meta data of the region processed in each traverses
+  let currentData = null;
 
-// Generate result data
-lines.forEach((lineStr, index) => {
-  // Whenever title is encountered, store previous region data
-  if (/Region/i.test(lineStr)) {
-    if (currentData) {
+  // The result data for generating output.csv
+  const resultData = [];
+
+  // Generate result data
+  lines.forEach((lineStr, index) => {
+    // Whenever title is encountered, store previous region data
+    if (/Region/i.test(lineStr)) {
+      if (currentData) {
+        resultData.push(currentData);
+      }
+
+      // Init for each region data
+      currentTitle = getTitle(lineStr);
+      currentData = {
+        title: currentTitle,
+        xyList: {
+          x: [],
+          y: [],
+        }
+      };
+    }
+
+    // Ignore lines staring with hash
+    if (lineStr.startsWith('#')) {
+      return;
+    }
+
+    // Fill region data
+    const xyArr = getXY(lineStr);
+    if (xyArr.length >= 2 && currentData) {
+      currentData.xyList.x.push(xyArr[0]);
+      currentData.xyList.y.push(xyArr[1]);
+    }
+
+    // Store the last region data
+    if (index === lines.length - 1) {
       resultData.push(currentData);
     }
 
-    // Init for each region data
-    currentTitle = getTitle(lineStr);
-    currentData = {
-      title: currentTitle,
-      xyList: {
-        x: [],
-        y: [],
-      }
-    };
-  }
-
-  // Ignore lines staring with hash
-  if (lineStr.startsWith('#')) {
-    return;
-  }
-
-  // Fill region data
-  const xyArr = getXY(lineStr);
-  if (xyArr.length >=2 && currentData) {
-    currentData.xyList.x.push(xyArr[0]);
-    currentData.xyList.y.push(xyArr[1]);
-  }
-
-  // Store the last region data
-  if (index === lines.length - 1) {
-    resultData.push(currentData);
-  }
-
-});
-
-
-
-// console.log(resultData);
-
-// The title line string
-const titleLine = resultData.map(item => item.title).join(',,') + ',';
-
-// The subtitle line, ie., x,y,x,y...
-const subtitleLine = new Array(resultData.length).fill('x,y').join(',');
-
-console.log(titleLine);
-
-// Max row number among all regions
-const maxRowNumber = Math.max(...resultData.map(item => item.xyList.x.length));
-
-console.log('max row number is', maxRowNumber);
-
-// Generate content lines for csv
-const contentLines = [];
-for (let i = 0; i < maxRowNumber; i++) {
-  let xyNumberList = [];
-  resultData.forEach((item) => {
-    xyNumberList.push(item.xyList.x[i] || '');
-    xyNumberList.push(item.xyList.y[i] || '');
   });
-  contentLines.push(xyNumberList.join(','));
+
+
+
+  // console.log(resultData);
+
+  // The title line string
+  const titleLine = resultData.map(item => item.title).join(',,') + ',';
+
+  // The subtitle line, ie., x,y,x,y...
+  const subtitleLine = new Array(resultData.length).fill('x,y').join(',');
+
+  console.log(titleLine);
+
+  // Max row number among all regions
+  const maxRowNumber = Math.max(...resultData.map(item => item.xyList.x.length));
+
+  console.log('max row number is', maxRowNumber);
+
+  // Generate content lines for csv
+  const contentLines = [];
+  for (let i = 0; i < maxRowNumber; i++) {
+    let xyNumberList = [];
+    resultData.forEach((item) => {
+      xyNumberList.push(item.xyList.x[i] || '');
+      xyNumberList.push(item.xyList.y[i] || '');
+    });
+    contentLines.push(xyNumberList.join(','));
+  }
+
+  if (!directoryExists('./output')) {
+    fs.mkdirSync('output');
+  }
+
+  const fileNameWithoutSuffix = path.parse(fileName).name;
+
+  // fs.writeFileSync(`./output/${fileNameWithoutSuffix}.json`, JSON.stringify(resultData, null, 2));
+
+  fs.writeFileSync(`./output/${fileNameWithoutSuffix}.csv`, [titleLine, subtitleLine, ...contentLines].join('\n'));
+}
+
+function processCSVFilesInDirectory(directoryPath) {
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error('Fail reading directory', err);
+      return;
+    }
+
+    files.forEach((fileName) => {
+      if (fileName.endsWith('.xy')) {
+        processSingleFile(fileName);
+      }
+    });
+  });
 }
 
 
-fs.writeFileSync('./output.json', JSON.stringify(resultData, null, 2));
+// Main...
+const inputDirectory = './input';
 
-fs.writeFileSync('./output.csv', [titleLine, subtitleLine, ...contentLines].join('\n'));
+if (directoryExists(inputDirectory)) {
+  processCSVFilesInDirectory(inputDirectory);
+} else {
+  console.error('input does not exist');
+}
+
+
+
+
+
+
+
+
